0162744ef8fed7c6fe4bcba753ec517076880c10
vishalanand768 <vishalanand768@yahoo.com>
2019-12-22 20:49:24 +0530

    Add project files.

---
+++ AddImage.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddImage.aspx.cs" Inherits="browse_image.AddImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                   <td>Name :</td>
+                    <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                   <td>Image Upload :</td>
+                    <td><asp:FileUpload ID="fui" runat="server"></asp:FileUpload></td>
+                </tr>
+
+                 <tr>
+                   <td></td>
+                    <td><asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label></td>
+                </tr>
+
+                <tr>
+                   <td></td>
+                    <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click"></asp:Button></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:TextBox ID="txtStartDate" runat="server" TextMode="Date"></asp:TextBox>
+                                      To
+                      <asp:TextBox ID="txtEndDate" runat="server" TextMode="Date"></asp:TextBox>
+                      <asp:Button ID="btnsearch" runat="server" Text="Search" OnClick="btnsearch_Click" /></td>
+                </tr>
+
+                <tr>
+                   <td></td>
+                    <td><asp:GridView ID="grd" runat="server" AutoGenerateColumns="false" OnRowCommand="grd_RowCommand">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Name">
+                                <ItemTemplate>
+                                    <%#Eval("name") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                             <asp:TemplateField HeaderText="Profile Pic">
+                                <ItemTemplate>
+                                    <asp:Image ID="img1" runat="server" width="60px" Height="40px" ImageUrl='<%#Eval("img","~/Pics/{0}") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <asp:TemplateField HeaderText="Action">
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="btndelete" runat="server" Text="Delete" CommandName="A" CommandArgument='<%#Eval("id") %>' /> |
+                                    <asp:LinkButton ID="btnedit" runat="server" Text="Edit" CommandName="B" CommandArgument='<%#Eval("id") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                        </asp:GridView></td>
+                </tr>
+
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AddImage.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace browse_image
+{
+    public partial class AddImage : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["xyz"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();
+            }
+        }
+
+        public void BindData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_emp", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@action", "select");
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            string FN = "";
+            string Ext = "";
+            if (btnsave.Text == "Save")
+            {
+                FN = DateTime.Now.Ticks.ToString() + Path.GetFileName(fui.PostedFile.FileName);
+                Ext = Path.GetExtension(fui.PostedFile.FileName);
+                if (Ext == ".jpeg" || Ext == ".jpg" || Ext == ".png")
+                {
+                    fui.SaveAs(Server.MapPath("Pics" + "\\" + FN));
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("usp_emp", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@action", "insert");
+                    cmd.Parameters.AddWithValue("@name", txtname.Text);
+                    cmd.Parameters.AddWithValue("@img", FN);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    BindData();
+                    lblmsg.Text = "Record saved!!!";
+                }
+                else
+                {
+                    lblmsg.Text = "Please upload only .jpeg,.jpg,.png files!!! ";
+                }
+            }
+            else
+            {
+                FN = Path.GetFileName(fui.PostedFile.FileName);
+               
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_emp", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "update");
+                cmd.Parameters.AddWithValue("@id", ViewState["PP"]);
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                if (FN != "")
+                {
+                    FN = DateTime.Now.Ticks.ToString() + FN;
+                    cmd.Parameters.AddWithValue("@img", FN);
+                    fui.SaveAs(Server.MapPath("Pics" + "\\" + FN));
+                    File.Delete(Server.MapPath("Pics" + "\\" + ViewState["KK"]));
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@img", ViewState["KK"]);
+                }
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindData();
+            }
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_emp", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "delete");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindData();
+            }
+            else if (e.CommandName == "B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_emp", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "edit");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0][1].ToString();
+                ViewState["KK"] = dt.Rows[0][2].ToString();
+                btnsave.Text = "Update";
+                ViewState["PP"] = e.CommandArgument;
+            }
+        }
+
+        protected void btnsearch_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_emp", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@a", Convert.ToDateTime(txtStartDate.Text));
+            cmd.Parameters.AddWithValue("@b", Convert.ToDateTime(txtEndDate.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+    }
+}
